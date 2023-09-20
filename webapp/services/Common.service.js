e4ee995607c23f376e881eba904423de64ec0ebf
sagarLDNA <sagar.p@logicaldna.com>
2023-09-20 14:49:22 +0530

    project activity screen development

--- webapp/services/Common.service.js
+++ webapp/services/Common.service.js
@@ -795,8 +795,13 @@ sap.ui.define([
             getAllContacts: function(callback){
                 this.runJQueryX("GET", "contact/search/" + this.session("companyId"), null, callback, null);
             },
+            
             getAllSubcontractors: function(callback){
                 this.runJQueryX("GET", "subcontractor/searchsubcontractors/" + this.session("companyId"), null, callback, null);
+            },
+
+            getQcCheckList : function(callback){
+                this.runJQueryX("GET", "common/getqcchecklistsearch/" + this.session("companyId"), null, callback, null);
             },
         };
     }
---
+++ webapp/services/ProjectManagement/QCCheckList.service.js
@@ -0,0 +1,29 @@
+sap.ui.define([
+    'sap/ui/elev8rerp/componentcontainer/services/Common.service',
+],
+    function (commonService) {
+        "use strict";
+
+        return {
+            /**
+               * @public
+               * @param {boolean} bIsPhone the value to be checked
+               * @returns {string} path to image
+            */
+  
+            getAllQcchecklist: function(callback){
+                commonService.runJQueryX("GET", "qcchecklist/searchqcchecklist/" + commonService.session("companyId"), null, callback, null);
+            },
+           
+            getQcchecklist: function(params, callback){
+                commonService.runJQueryX("GET", "qcchecklist/selectqcchecklist/" + params.id, null, callback, null);
+            },
+
+            saveQcchecklist: function(params, callback){
+                commonService.runJQueryX("POST", "qcchecklist/saveqcchecklist" , params, callback, null);
+            }
+
+
+        };
+    }
+);
